f096936c10afa4bed4bb2b0984f95cf9572d7e1d
LEA_HASELSTEINER <leahaselsteiner@hotmail.com>
2024-10-14 17:21:17 +0200

    excercise 3

--- basic.js
+++ basic.js
@@ -5,8 +5,19 @@ console.log(helloWorld);
 
 // exercise 2
 let age = '29';
-let name = 'Lea';
+const name = 'Lea';
 let favoriteColor = 'blue';
 console.log(`Hi my name is ${name}, I am ${age} years old and my favoriteColor is ${favoriteColor}.`);
 
 // exercise 3
+let a = 31;
+let b = 15;
+
+// addition
+console.log(a + b);
+// subraction
+console.log(a - b);
+// multiplication
+console.log(b * a);
+// division
+console.log(b / a);

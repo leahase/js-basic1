47ba3d67dfb622a3cb0547372c3cb5c7c8d81b5b
LEA_HASELSTEINER <leahaselsteiner@hotmail.com>
2024-10-14 17:08:30 +0200

    exercise 2

--- basic.js
+++ basic.js
@@ -1,5 +1,12 @@
 // exercise 1
 
-let helloWorld = "Hello, World!"
+let helloWorld = "Hello, World!";
 console.log(helloWorld);
 
+// exercise 2
+let age = '29';
+let name = 'Lea';
+let favoriteColor = 'blue';
+console.log(`Hi my name is ${name}, I am ${age} years old and my favoriteColor is ${favoriteColor}.`);
+
+// exercise 3
